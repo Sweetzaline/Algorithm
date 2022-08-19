1dc0d28ded459c3479c2366e688dbda371d97087
unknown <buragohain4@gmail.com>
2022-08-19 22:05:38 +0530

    Adding Question 2

---
+++ Q2/Q2/BinarySearch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+
+{
+    class BinarySearch
+    {
+        public static void BinarySearchWord(List<string> wordList)
+        {
+            ///Initialize Local Variables
+            int mid, start = 0;
+            int end = wordList.Count - 1;
+            bool isFound = false;
+            Console.WriteLine("Enter the word to be searched in the text file.");
+            string key = Console.ReadLine();
+
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                int result = key.CompareTo(wordList[mid]);
+                if (result == 0)
+                {
+                    Console.WriteLine("The word is Present in the text file.");
+                    isFound = true;
+                    break;
+                }
+                else if (result > 0)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("The word does not exist in the text file!");
+            }
+
+        }
+    }
+}
---
+++ Q2/Q2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            Console.WriteLine("Q2.Binary Search the word from wordlist");
+            Console.WriteLine("Enter the Question No.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                ///For Question 2
+                case 2:
+                    string txtpath = File.ReadAllText(@"C:\Users\Amit Pegu\Desktop\Algorithm\Q2\Algorithm.txt");
+                    List<string> words = new List<string>(txtpath.Split());
+                    words.Sort();
+                    BinarySearch.BinarySearchWord(words);
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the valid option!");
+                    break;
+            }
+            Console.WriteLine("\nThank You!");
+            Console.ReadLine();
+        }
+    }
+
+}
