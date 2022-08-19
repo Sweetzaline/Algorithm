c7db75a5d6891013da89e9c00ac5318adc856afe
unknown <buragohain4@gmail.com>
2022-08-19 23:54:50 +0530

    Adding Question No. 8

---
+++ Q8/Q8/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Q8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            Console.WriteLine("8.Prime numbers with anagrams and palindrome");
+            Console.WriteLine("Enter the Question No."); ;
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                /// For Question No. 8
+                case 8:
+                    Console.WriteLine("Enter First number:");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter Second Number :");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+
+                    int res1 = PrimeNumExtend.FindPrime(num1);
+                    int res2 = PrimeNumExtend.FindPrime(num2);
+
+                    bool anagramRes = PrimeNumExtend.FindAnagram(Convert.ToString(num1), Convert.ToString(num2));
+
+                    bool palindormres = PrimeNumExtend.IsPalindrome(num1);
+
+                    if (anagramRes)
+                    {
+                        Console.WriteLine("both values are anagrams");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("both the values are not anagrams");
+                    }
+                    if (palindormres)
+                    {
+                        Console.WriteLine("Number is palindrome");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Number is not a palindrome");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the correct Question No.!");
+                    break;
+            }
+            Console.WriteLine("\nThank You!");
+            Console.ReadLine();
+        }
+    }
+
+}
