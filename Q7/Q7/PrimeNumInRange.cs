1ea1a354931f93f4faf97ce39922583d08eb53af
unknown <buragohain4@gmail.com>
2022-08-19 23:41:28 +0530

    Adding Question No. 7

---
+++ Q7/Q7/PrimeNumInRange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Q7
+{
+    class PrimeNumInRange
+    {
+        int end = 1000, count = 0;
+        public void FindPrime()
+        {
+            for (int i = 1; i < end; i++)
+            {
+                for (int j = 1; j < end; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.Write(i + " ");
+                }
+                count = 0;
+            }
+        }
+    }
+}
---
+++ Q7/Q7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Q7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            Console.WriteLine("7.Prime number in range");
+            Console.WriteLine("Enter the Question No."); ; 
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                 ///For UseCase7
+                case 7:
+                    Console.WriteLine("The prime numbers in the range of 1-1000 are : ");
+                    PrimeNumInRange primeNum = new PrimeNumInRange();
+                    primeNum.FindPrime();
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the correct Question No.!");
+                    break;
+            }
+            Console.WriteLine("\nThank You!");
+            Console.ReadLine();
+        }
+    }
+
+}
