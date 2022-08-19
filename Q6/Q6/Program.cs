a8a520c404e20defb6ddbbdd894e13f32f2ac58a
unknown <buragohain4@gmail.com>
2022-08-19 23:31:16 +0530

    Adding Question 6

---
+++ Q6/Q6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            Console.WriteLine("6.Anagram Detection Example");
+            Console.WriteLine("Enter the Question No."); ;
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+               ///For UseCase6
+                case 6:
+                    AnagramDetection.FindAnagram();
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the correct Question No.!");
+                    break;
+            }
+            Console.WriteLine("\nThank You!");
+            Console.ReadLine();
+        }
+    }
+
+}
